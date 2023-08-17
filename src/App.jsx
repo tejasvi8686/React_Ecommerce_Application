b93ceb35a5adb4a8bf5d8ba4bef4b7bdd8a103d6
tejasvi8686 <tejasviraj8686@gmail.com>
2023-08-17 10:49:06 +0530

    added prod

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,28 @@ import Cart from "./pages/cart";
 import Login from "./pages/login";
 import Wishlist from "./pages/wishlist";
 import "./App.css";
+import ShopContext from "./components/shopcontext";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="shop" element={<Shop />} />
-            <Route path="blog" element={<Blog />} />
-            <Route path="about" element={<About />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="cart" element={<Cart />} />
-            <Route path="login" element={<Login />} />
-            <Route path="wishlist" element={<Wishlist />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+    <div>
+      <ShopContext>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="shop" element={<Shop />} />
+              <Route path="blog" element={<Blog />} />
+              <Route path="about" element={<About />} />
+              <Route path="contact" element={<Contact />} />
+              <Route path="cart" element={<Cart />} />
+              <Route path="login" element={<Login />} />
+              <Route path="wishlist" element={<Wishlist />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ShopContext>
+    </div>
   );
 }
 
--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import Shopitems from '../components/shopitems';
 
 const shop = () => {
   return (
-    <div>shop</div>
-  )
-}
+    <div>
+      <section className="shop-banner p-5">
+        <div className="container-xxl">
+          <div className="row justify-content-center align-items-center">
+            <div className="col-md-8 col-lg-6 text-center">
+              <div className="shop-details">
+                <h1 className="text-white">
+                  <b className="title-green">#100%</b> Off On All Products
+                </h1>
+                <p className="text-white fs-5">
+                  Make your orders we will deliver..
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
-export default shop
+      <section className="featured-products p-5">
+        <div className="container-xxl">
+          <div className="row">
+            <Shopitems />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default shop;
