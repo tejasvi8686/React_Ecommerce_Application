e64e3056de20b4b1727e6b1a9d997eafff9cefb6
tejasvi8686 <tejasviraj8686@gmail.com>
2023-08-17 21:42:17 +0530

    added contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,9 +1,75 @@
 import React from 'react'
 
 const contact = () => {
-  return (
-    <div>contact</div>
-  )
+  return <>
+   <section className="contact-wrapper p-5">
+    <div className="container-xxl">
+      <div className="row">
+      <div className="col-12 text-center">
+            <h1 className=" text">Reach Out to Us</h1>
+            <p className=' text fs-3'>We are only a step away from you</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div className="contact-wrapper-details p-5">
+    <div className="container-xxl">
+      <div className="row text-center align-items-center">
+        <div className="col-lg-6 col-md-12 p-3">
+          <div className="map card m-auto embed-responsive embed-responsive-16by9">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3495.0842478816844!2d77.58221447470171!3d28.836351674797253!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390c8aa1fd6a7f25%3A0x30d72f474b492740!2sDr.%20K.N.%20Modi%20Engineering%20College!5e0!3m2!1sen!2sin!4v1692288359778!5m2!1sen!2sin" allowfullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+        </div>
+        <div className="col-lg-6 col-md-12">
+            <div className="card p-5">
+                <h2 className='footer-title mb-3'><b>Contact Us</b></h2>
+                  <p className='mb-2'><b>Address:</b>  Dr. K.N. Modi Engineering College</p>
+                  <p className='mb-2'><b>Phone:</b>  <a className='footer-tel' href="tel:+1234567890">Call us at +1 (234) 567-890</a></p>
+                  <p className='mb-4'><b>Hours:</b>  From 9 a.m To 5 p.m</p>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus quos iusto rerum architecto a eaque consequuntur impedit! Harum earum iste, suscipit soluta, culpa necessitatibus quia sit nulla doloremque officia cum.</p>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit hic veniam unde numquam in ullam laudantium odit explicabo itaque! Voluptate similique, accusantium consequatur provident soluta quaerat maxime adipisci vero sed.</p>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+
+  <div className="contact-message p-5">
+    <div className="container-xxl">
+      <div className="row">
+        <div className="col-lg-8 col-md-10 col-sm-12 m-auto">
+        <div className="card p-5">
+        <h2 className='text-center mb-4'>Leave Us A message</h2>
+        <div className=" d-flex align-items-center justify-content-center">
+        <div className="row g-3">
+              <div className="col-md-6">
+                <input type="text" className="form-control" placeholder="First name" aria-label="First name" />
+              </div>
+              <div className="col-md-6">
+                <input type="text" className="form-control" placeholder="Last name" aria-label="Last name" />
+              </div>
+              <div className='col-12'>
+              <label htmlFor="exampleFormControlInput1" className="form-label mb-3">Enter Your Email address</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Email" />
+                   
+              </div>
+              <div className="col-12">
+                  <label htmlFor="exampleFormControlTextarea1" className="form-label">Type in your message</label>
+                  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <div className='col-12 text-center gap-2'>
+                <button id='button-link' type="submit">Submit</button>
+                </div>
+            </div>
+          </div>
+        </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </>;
 }
 
 export default contact
+
+
